78a99220675b0c6bd1b99e1c90471ed52d230932
Tetsuro Oguro <git@ogu.com>
2019-05-01 18:30:56 +0900

    Initial Commit

---
+++ CppDesignPattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/05/01.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
