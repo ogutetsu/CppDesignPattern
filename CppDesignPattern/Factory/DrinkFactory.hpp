0b911161d3dd4c7caac3d9f880ad815475ff4a78
Tetsuro Oguro <git@ogu.com>
2019-05-23 05:58:45 +0900

    FunctionalFactoryクラスを追加

--- CppDesignPattern/Factory/DrinkFactory.hpp
+++ CppDesignPattern/Factory/DrinkFactory.hpp
@@ -12,6 +12,7 @@
 #include "HotDrink.hpp"
 #include "HotDrinkFactory.hpp"
 #include <map>
+#include <functional>
 using namespace std;
 
 class DrinkFactory
@@ -28,6 +29,25 @@ public:
         auto drink = hotFactory[name]->make();
         drink->prepare(200);
         return drink;
+    }
+};
+
+class DrinkWithVolumeFactory
+{
+    map<string, function<unique_ptr<HotDrink>()>> factories;
+public:
+    DrinkWithVolumeFactory()
+    {
+        factories["tea"] = [] {
+            auto tea = make_unique<Tea>();
+            tea->prepare(200);
+            return tea;
+        };
+    }
+    
+    unique_ptr<HotDrink> make_drink(const string& name)
+    {
+        return factories[name]();
     }
 };
 
