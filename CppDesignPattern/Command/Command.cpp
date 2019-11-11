9ef25056142c737993867e8a913b52a821f44601
Tetsuro Oguro <git@ogu.com>
2019-11-12 08:26:32 +0900

    Commandの追加

---
+++ CppDesignPattern/Command/Command.cpp
@@ -0,0 +1,93 @@
+//
+//  Command.cpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/11/12.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#include "Command.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct BankAccount
+{
+    int balance{0};
+    int overdraft_limit{-500};
+    
+    void deposit(int amount)
+    {
+        balance += amount;
+        cout << "deposited " << amount << ", balance is now " << balance << endl;
+    }
+    
+    void withdraw(int amount)
+    {
+        if(balance - amount >= overdraft_limit)
+        {
+            balance -= amount;
+            cout << "withdraw " << amount << ", balance is now " << balance << endl;
+        }
+    }
+    
+    friend ostream& operator <<(ostream& os, const BankAccount& account)
+    {
+        os << "balance: " << account.balance;
+        return os;
+    }
+};
+
+struct Command
+{
+    virtual void call() = 0;
+};
+
+struct BankAccountCommand : Command
+{
+    BankAccount& account;
+    enum Action { deposit, withdraw } action;
+    int amount;
+    
+    BankAccountCommand(BankAccount& account, Action action, int amount) :
+    account(account), action(action), amount(amount) {}
+    
+    void call() override
+    {
+        switch(action)
+        {
+            case deposit:
+                account.deposit(amount);
+                break;
+            case withdraw:
+                account.withdraw(amount);
+                break;
+        }
+    }
+};
+
+
+
+void CommandMain()
+{
+    BankAccount ba;
+    vector<BankAccountCommand> commands
+    {
+        BankAccountCommand{ba, BankAccountCommand::deposit, 100},
+        BankAccountCommand{ba, BankAccountCommand::withdraw, 200}
+    };
+    
+    cout << ba << endl;
+    
+    for(auto& cmd : commands)
+    {
+        cmd.call();
+    }
+    
+    
+    cout << ba << endl;
+    
+}
+
---
+++ CppDesignPattern/Command/Command.hpp
@@ -0,0 +1,14 @@
+//
+//  Command.hpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/11/12.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#ifndef Command_hpp
+#define Command_hpp
+
+#include <stdio.h>
+
+#endif /* Command_hpp */
