175fa08a5f9ac956147e94c17b445be39724a601
Tetsuro Oguro <git@ogu.com>
2019-11-12 08:26:21 +0900

    PointerChainのコミット忘れ

---
+++ CppDesignPattern/Responsibility/PointerChain.cpp
@@ -0,0 +1,110 @@
+//
+//  PointerChain.cpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/10/29.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#include "PointerChain.hpp"
+#include <ostream>
+#include <iostream>
+
+using namespace std;
+
+
+struct Creature
+{
+    string name;
+    int attack, defense;
+    
+    Creature(const string& name, int attack, int defense) :
+    name(name), attack(attack), defense(defense) {}
+    
+    friend ostream& operator <<(ostream& os, const Creature& creature){
+        os << "name: " << creature.name << " attack: " << creature.attack << " defense: " << creature.defense;
+        return os;
+    }
+    
+    
+};
+
+
+class CreatureModifier
+{
+    CreatureModifier* next{nullptr};
+    
+protected:
+    Creature& creature;
+public:
+    CreatureModifier(Creature& creature) : creature(creature) {}
+    
+    void add(CreatureModifier* cm)
+    {
+        if(next) next->add(cm);
+        else next = cm;
+    }
+    
+    virtual void handle()
+    {
+        if(next) next->handle();
+    }
+    
+};
+
+
+class DoubleAttackModifier : public CreatureModifier
+{
+public:
+    DoubleAttackModifier(Creature& creature) : CreatureModifier(creature) {}
+    void handle() override {
+        creature.attack *= 2;
+        CreatureModifier::handle();
+    }
+};
+
+class IncreaseDefenseModifier : public CreatureModifier
+{
+public:
+    IncreaseDefenseModifier(Creature& creature) : CreatureModifier(creature) {}
+    void handle() override {
+        if(creature.attack <= 2)
+        {
+            creature.defense++;
+        }
+        CreatureModifier::handle();
+    }
+};
+
+class NoBonusesModifier : public CreatureModifier
+{
+public:
+    NoBonusesModifier(Creature& creature) : CreatureModifier(creature) {}
+    
+    void handle() override {
+        
+    }
+    
+};
+
+void PointerChainMain()
+{
+    Creature goblin{"Goblin", 1, 1};
+    CreatureModifier root{goblin};
+    DoubleAttackModifier r1{goblin};
+    DoubleAttackModifier r1_2{goblin};
+    IncreaseDefenseModifier r2{goblin};
+    
+    NoBonusesModifier curse{goblin};
+    root.add(&curse);
+    
+    root.add(&r1);
+    root.add(&r1_2);
+    root.add(&r2);
+    
+    root.handle();
+    
+    cout << goblin << endl;
+    
+}
+
---
+++ CppDesignPattern/Responsibility/PointerChain.hpp
@@ -0,0 +1,14 @@
+//
+//  PointerChain.hpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/10/29.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#ifndef PointerChain_hpp
+#define PointerChain_hpp
+
+#include <stdio.h>
+
+#endif /* PointerChain_hpp */
