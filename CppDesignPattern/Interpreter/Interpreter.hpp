b9860976768eb046b26e635e400fdd0bf937906d
Tetsuro Oguro <git@ogu.com>
2019-11-22 20:24:22 +0900

    Interpreterの追加

---
+++ CppDesignPattern/Interpreter/Interpreter.hpp
@@ -0,0 +1,14 @@
+//
+//  Interpreter.hpp
+//  CppDesignPattern
+//
+//  Created by ogurotetsuro on 2019/11/22.
+//  Copyright © 2019 ogurotetsuro. All rights reserved.
+//
+
+#ifndef Interpreter_hpp
+#define Interpreter_hpp
+
+#include <stdio.h>
+
+#endif /* Interpreter_hpp */
